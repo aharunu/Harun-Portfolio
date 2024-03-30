5b51776b39164e7b4a25eacd2a55493163632c73
Muhtalip Dede <muhtalipdede@gmail.com>
2024-03-30 17:24:52 +0300

    Fix typos and update text formatting

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -2,7 +2,7 @@ const PortfolioPage = () => {
     return (
         <div>
             <h1 className="text-2xl md:text-3xl font-bold">Readability Assessment Tool</h1>
-            <h3 className="pl-8">A Python tool for analyzing text extracted from PDF file. I created this project for CS50's final assignment.</h3>
+            <h3 className="pl-8">A Python tool for analyzing text extracted from PDF file. I created this project for CS50&apos;s final assignment.</h3>
             <div className="justify-center">
                 <iframe width="1600" height="600" src="https://www.youtube.com/embed/WSmdCzWLnOc" title="CS50 Final Project - Analyzing Text from PDF Files in Python | Readability Assessment Tool" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; fullscreen; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
