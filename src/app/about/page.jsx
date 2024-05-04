a73e44c686ed34d9d23c30e2ee0cce85eebd555b
Muhtalip Dede <muhtalipdede@gmail.com>
2024-05-04 17:37:39 +0300

    chore: Fix typos and update text formatting in code files

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,13 @@
 const AboutPage = () => {
     return (
-        <div className="h-1/2 flex flex-col w-1/3 font-bold justify-center text-white">
-            <h1 className="text-3xl pl-8 pb-8">Biography</h1>
-            <h3 className="pl-10 ">I&apos;am Harun. I&apos;am studying at Yildiz Technical University. During my university years
+        <div className="flex flex-col items-center">
+            <h1 className="text-3xl px-8 pb-8">Biography</h1>
+            <h2 className="px-10 text-lg">
+                I&apos;am Harun. I&apos;am studying at Yildiz Technical University. During my university years
                 I&apos;am trying to learn concepts of computer science and software development. Exploring various programming languages,
                 algorithms, and technologies Through challenging lectures, online courses, and extracurricular activities, 
-                I sharpened my skills in coding, problem-solving, and critical thinking. </h3>
+                I sharpened my skills in coding, problem-solving, and critical thinking.
+            </h2>
         </div>
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -7,20 +7,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portfolio",
   description: "My personal portfolio website.",
+  url: "https://harundev.vercel.app/portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-gradient-to-b from-blue-400 to-black">
+        {/* TODO: */}
+        <div className="w-screen h-max bg-gradient-to-b from-blue-400 to-black">
           <div className="h-24">
             <Navbar />
           </div>
           <div className="h-[calc(100vh-6rem)]">
-          {children}
+            {children}
           </div>
-
         </div>
       </body>
     </html>
